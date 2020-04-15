c9446b59104aadee1c957be1225b60f4207fab0d
BockSong <songrcs@gmail.com>
2020-04-14 21:00:37 -0400

    work on basic 2PC structure

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,55 @@
-/* Skeleton code for Server */
+/* 
+ * 
+ * Name: Rong Song
+ * Andrew ID: rongsong
+ *
+ * Server.java - Implementation of coordinator in 2PC protocol.
+ * 
+ * The server class for two-phase commit for group photo collage.
+ * 
+ */
+
+import java.io.*;
+import java.util.concurrent.*;
 
 public class Server implements ProjectLib.CommitServing {
 	
+	// map the ...
+	private static ConcurrentHashMap<Integer, String> child_role = new 
+							ConcurrentHashMap<Integer, String>();
+	private static Boolean DEBUG = true; 
+
+	/*
+	 * send: 
+	 */
+	private static void send() {
+		;
+	}
+
+	/*
+	 * startCommit: This method is called when a new candidate collage has been
+	 * posted, and it should cause a new 2PC commit operation to begin.
+	 * @filename: Filename of the collage.
+	 * @img: Contents of the collage.
+	 * @sources: Source images contributed to the collage, in forms of "Nodeidx:
+	 * 			 filename"
+	 */
 	public void startCommit( String filename, byte[] img, String[] sources ) {
 		System.out.println( "Server: Got request to commit "+filename );
+		// initiates a 2PC procedure
+		int i, num_src;
+		num_src = sources.length;
+		String[] srcID = new String[num_src];
+		String[] srcName = new String[num_src];
+		for (i = 0; i < num_src; i++) {
+			// get the node ID and image name from each source
+			srcID[i] = sources[i].substring(0, sources[i].indexOf(":"));
+			srcName[i] = sources[i].substring(sources[i].indexOf(":") + 1, sources[i].length());
+			
+			// TODO: send inquiries to each involved site
+		}
+
+		// TODO: (after 2PC done) generate composite images in the Server directory
 	}
 	
 	public static void main ( String args[] ) throws Exception {
--- src/UserNode.java
+++ src/UserNode.java
@@ -1,20 +1,54 @@
-/* Skeleton code for UserNode */
+/* 
+ *
+ * Name: Rong Song
+ * Andrew ID: rongsong
+ *
+ * UserNode.java 
+ * 
+ * The usernode (client) class for two-phase commit for group photo collage.
+ * 
+ */
 
 public class UserNode implements ProjectLib.MessageHandling {
 	public final String myId;
+	public static ProjectLib PL;
+
 	public UserNode( String id ) {
 		myId = id;
 	}
 
+	/*
+	 * deliverMessage: Callback called when message received.
+	 * Return: True if the message is handled, or False if not.
+	 */
 	public boolean deliverMessage( ProjectLib.Message msg ) {
 		System.out.println( myId + ": Got message from " + msg.addr );
+		if (msg.addr.equals("Server")) {
+			// TODO: if it's inquiry
+			// TODO: how to receive img from the server?
+			byte[] img;
+			String[] sources;
+			if (PL.askUser(img, sources)) {
+				// acceptable, return ack msg
+				;
+			}
+			else {
+				// unacceptable, return reject msg
+				;
+			}
+
+			// TODO: if it's informing the success of commitment
+			// remove sources images from the UserNode directories
+			// perform other actions after commitment
+		}
+
 		return true;
 	}
 	
 	public static void main ( String args[] ) throws Exception {
 		if (args.length != 2) throw new Exception("Need 2 args: <port> <id>");
 		UserNode UN = new UserNode(args[1]);
-		ProjectLib PL = new ProjectLib( Integer.parseInt(args[0]), args[1], UN );
+		PL = new ProjectLib( Integer.parseInt(args[0]), args[1], UN );
 		
 		ProjectLib.Message msg = new ProjectLib.Message( "Server", "hello".getBytes() );
 		System.out.println( args[1] + ": Sending message to " + msg.addr );
